dd97758f959aeb8487eb6d08e3b309389e47500a
sanjaykumarsm <sanjay.k@xoxoday.com>
2021-11-02 20:41:45 +0530

    log

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const { createLogger, format, transports } = require('winston');
 const rTracer = require('cls-rtracer');
 
 module.exports = createLogger({
-transports:
+transports:[
     new transports.File({
     filename: 'logs/server.log',
     format:format.combine(
@@ -10,4 +10,11 @@ transports:
         format.align(),
         format.printf(info => `${rTracer.id()}: ${info.level}: ${[info.timestamp]}: ${info.message}`),
     )}),
+    new transports.Console({
+        format:format.combine(
+            format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+            format.align(),
+            format.printf(info => `${rTracer.id()}: ${info.level}: ${[info.timestamp]}: ${info.message}`),
+        )})
+    ]
 });
